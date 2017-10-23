526318ae6e8b4e5b94242452e17936c5cde55bc9
bunhan.kisra <bunhan.n@kisracorp.com>
2017-10-23 14:55:05 +0700

    v2

--- robot.ts
+++ robot.ts
@@ -1,6 +1,12 @@
+ namespace bunCB {
+	 /************************************************************************************************************************************************
+	* micro:bit Servo:Lite / :MOVE mini blocks
+	************************************************************************************************************************************************/
  /**
      * Drives forwards. Call stop to stop
      */
+    //% subcategory=ServoLite
+    //% blockId=kitronik_servolite_servos_forward
     //% block="drive forward"
     export function forward(): void {
         pins.digitalWritePin(DigitalPin.P16, 1)
@@ -8,4 +14,5 @@
 		pins.digitalWritePin(DigitalPin.P0, 0)
 		pins.digitalWritePin(DigitalPin.P1, 0)
     }
+ }
 	
