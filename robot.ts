32b46725333d1679f611212a7b63283913e08263
bunhan.kisra <bunhan.n@kisracorp.com>
2017-10-25 16:23:21 +0700

    Add drive back,left,right,stop

--- robot.ts
+++ robot.ts
@@ -14,5 +14,54 @@
 		pins.digitalWritePin(DigitalPin.P0, 0)
 		pins.digitalWritePin(DigitalPin.P1, 0)
     }
+	
+	/**
+     * Drives back. Call stop to stop
+     */
+    //% subcategory=RobotControl
+    //% blockId=RobotControl_backward
+    //% block="drive back"
+    export function backward(): void {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+		pins.digitalWritePin(DigitalPin.P15, 0)
+		pins.digitalWritePin(DigitalPin.P0, 1)
+		pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+	/**
+     * Drives spin left. Call stop to stop
+     */
+    //% subcategory=RobotControl
+    //% blockId=RobotControl_left
+    //% block="spin left"
+    export function spinLeft(): void {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+		pins.digitalWritePin(DigitalPin.P15, 1)
+		pins.digitalWritePin(DigitalPin.P0, 1)
+		pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+	/**
+     * Drives spin right. Call stop to stop
+     */
+    //% subcategory=RobotControl
+    //% blockId=RobotControl_right
+    //% block="spin right"
+    export function spinRight(): void {
+        pins.digitalWritePin(DigitalPin.P16, 1)
+		pins.digitalWritePin(DigitalPin.P15, 0)
+		pins.digitalWritePin(DigitalPin.P0, 0)
+		pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+	/**
+     * Drives stop
+     */
+    //% subcategory=RobotControl
+    //% blockId=RobotControl_stop
+    //% block="drive stop"
+    export function stop(): void {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+		pins.digitalWritePin(DigitalPin.P15, 0)
+		pins.digitalWritePin(DigitalPin.P0, 0)
+		pins.digitalWritePin(DigitalPin.P1, 0)
+    }
  }
 	
